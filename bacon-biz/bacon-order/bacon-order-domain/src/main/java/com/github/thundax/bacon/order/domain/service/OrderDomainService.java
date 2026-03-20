472c0b90038e972e5b3779dabecf4271ed70a74f
thundax-lyp <731378491@qq.com>
2026-03-20 18:43:35 +0800

    init

--- bacon-biz/bacon-order/bacon-order-domain/src/main/java/com/github/thundax/bacon/order/domain/service/OrderDomainService.java
+++ bacon-biz/bacon-order/bacon-order-domain/src/main/java/com/github/thundax/bacon/order/domain/service/OrderDomainService.java
@@ -4,7 +4,7 @@ import com.github.thundax.bacon.order.domain.model.entity.Order;
 
 public class OrderDomainService {
 
-    public Order create(Long id, String orderNo, String customerName) {
-        return new Order(id, orderNo, customerName);
+    public Order create(Long id, Long tenantId, String orderNo, Long userId, String customerName) {
+        return new Order(id, tenantId, orderNo, userId, customerName);
     }
 }

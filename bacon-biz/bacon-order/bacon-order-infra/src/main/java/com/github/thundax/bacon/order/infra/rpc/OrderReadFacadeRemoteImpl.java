0160d4e37109b6ab048edc962aea35d9a62895ce
thundax-lyp <731378491@qq.com>
2026-03-23 12:28:22 +0800

    init

--- bacon-biz/bacon-order/bacon-order-infra/src/main/java/com/github/thundax/bacon/order/infra/rpc/OrderReadFacadeRemoteImpl.java
+++ bacon-biz/bacon-order/bacon-order-infra/src/main/java/com/github/thundax/bacon/order/infra/rpc/OrderReadFacadeRemoteImpl.java
@@ -15,7 +15,7 @@ public class OrderReadFacadeRemoteImpl implements OrderReadFacade {
 
     private final RestClient restClient;
 
-    public OrderReadFacadeRemoteImpl(@Value("${bacon.remote.order-base-url:http://localhost:8081}") String baseUrl) {
+    public OrderReadFacadeRemoteImpl(@Value("${bacon.remote.order-base-url:http://localhost:8083}") String baseUrl) {
         this.restClient = RestClient.builder().baseUrl(baseUrl).build();
     }
 

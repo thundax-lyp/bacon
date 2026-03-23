20b1e8368beeceef1dee4eaa3434e746837eb3a6
thundax-lyp <731378491@qq.com>
2026-03-23 12:27:33 +0800

    init

---
+++ bacon-common/bacon-common-swagger/src/main/java/com/github/thundax/bacon/common/swagger/SwaggerProperties.java
@@ -0,0 +1,80 @@
+package com.github.thundax.bacon.common.swagger;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "bacon.swagger")
+public class SwaggerProperties {
+
+    private final Oauth2 oauth2 = new Oauth2();
+
+    public Oauth2 getOauth2() {
+        return oauth2;
+    }
+
+    public static class Oauth2 {
+
+        private boolean enabled = true;
+        private String authorizationUrl;
+        private String tokenUrl;
+        private String refreshUrl;
+        private String authorizationPath = "/oauth2/authorize";
+        private String tokenPath = "/oauth2/token";
+        private String refreshPath = "/auth/token/refresh";
+
+        public boolean isEnabled() {
+            return enabled;
+        }
+
+        public void setEnabled(boolean enabled) {
+            this.enabled = enabled;
+        }
+
+        public String getAuthorizationUrl() {
+            return authorizationUrl;
+        }
+
+        public void setAuthorizationUrl(String authorizationUrl) {
+            this.authorizationUrl = authorizationUrl;
+        }
+
+        public String getTokenUrl() {
+            return tokenUrl;
+        }
+
+        public void setTokenUrl(String tokenUrl) {
+            this.tokenUrl = tokenUrl;
+        }
+
+        public String getRefreshUrl() {
+            return refreshUrl;
+        }
+
+        public void setRefreshUrl(String refreshUrl) {
+            this.refreshUrl = refreshUrl;
+        }
+
+        public String getAuthorizationPath() {
+            return authorizationPath;
+        }
+
+        public void setAuthorizationPath(String authorizationPath) {
+            this.authorizationPath = authorizationPath;
+        }
+
+        public String getTokenPath() {
+            return tokenPath;
+        }
+
+        public void setTokenPath(String tokenPath) {
+            this.tokenPath = tokenPath;
+        }
+
+        public String getRefreshPath() {
+            return refreshPath;
+        }
+
+        public void setRefreshPath(String refreshPath) {
+            this.refreshPath = refreshPath;
+        }
+    }
+}

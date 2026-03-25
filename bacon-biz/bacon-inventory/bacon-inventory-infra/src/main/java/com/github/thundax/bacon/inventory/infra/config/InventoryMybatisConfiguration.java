5cadde3bf8d5021af64129f3f3c8c61e8bac2dec
thundax-lyp <731378491@qq.com>
2026-03-25 15:34:12 +0800

    Fix(inventory): 修复无SqlSessionFactory场景Mapper装配并补齐装配测试开关

--- bacon-biz/bacon-inventory/bacon-inventory-infra/src/main/java/com/github/thundax/bacon/inventory/infra/config/InventoryMybatisConfiguration.java
+++ bacon-biz/bacon-inventory/bacon-inventory-infra/src/main/java/com/github/thundax/bacon/inventory/infra/config/InventoryMybatisConfiguration.java
@@ -1,9 +1,12 @@
 package com.github.thundax.bacon.inventory.infra.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnBean(SqlSessionFactory.class)
 @MapperScan("com.github.thundax.bacon.inventory.infra.persistence.mapper")
 public class InventoryMybatisConfiguration {
 }

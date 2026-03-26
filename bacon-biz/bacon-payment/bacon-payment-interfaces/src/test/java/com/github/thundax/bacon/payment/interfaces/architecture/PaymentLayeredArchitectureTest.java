2194391c484be323d7848831de7066dcaa6bbaa4
thundax-lyp <731378491@qq.com>
2026-03-26 15:52:53 +0800

    Test(payment): 新增分层依赖ArchUnit校验

---
+++ bacon-biz/bacon-payment/bacon-payment-interfaces/src/test/java/com/github/thundax/bacon/payment/interfaces/architecture/PaymentLayeredArchitectureTest.java
@@ -0,0 +1,12 @@
+package com.github.thundax.bacon.payment.interfaces.architecture;
+
+import com.github.thundax.bacon.common.test.arch.LayeredArchitectureRuleSupport;
+import org.junit.jupiter.api.Test;
+
+class PaymentLayeredArchitectureTest {
+
+    @Test
+    void shouldFollowLayerDependencyDirection() {
+        LayeredArchitectureRuleSupport.assertDefaultDirection("com.github.thundax.bacon.payment");
+    }
+}

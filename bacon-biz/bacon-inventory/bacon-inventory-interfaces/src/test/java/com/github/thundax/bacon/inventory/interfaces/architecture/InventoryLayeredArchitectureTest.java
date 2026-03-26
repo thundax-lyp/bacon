a35d40662d32ead8071bd4755535099e046d7ff3
thundax-lyp <731378491@qq.com>
2026-03-26 15:48:41 +0800

    Test(inventory): 新增分层依赖ArchUnit校验

---
+++ bacon-biz/bacon-inventory/bacon-inventory-interfaces/src/test/java/com/github/thundax/bacon/inventory/interfaces/architecture/InventoryLayeredArchitectureTest.java
@@ -0,0 +1,12 @@
+package com.github.thundax.bacon.inventory.interfaces.architecture;
+
+import com.github.thundax.bacon.common.test.arch.LayeredArchitectureRuleSupport;
+import org.junit.jupiter.api.Test;
+
+class InventoryLayeredArchitectureTest {
+
+    @Test
+    void shouldFollowLayerDependencyDirection() {
+        LayeredArchitectureRuleSupport.assertDefaultDirection("com.github.thundax.bacon.inventory");
+    }
+}

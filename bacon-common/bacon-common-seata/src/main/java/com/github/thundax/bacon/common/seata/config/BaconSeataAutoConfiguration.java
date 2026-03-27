9b51f81e4fe397dd71533a4e60ab7f70ec11f73c
thundax-lyp <731378491@qq.com>
2026-03-27 14:51:13 +0800

    Refactor(common): 统一Seata自动装配声明

--- bacon-common/bacon-common-seata/src/main/java/com/github/thundax/bacon/common/seata/config/BaconSeataAutoConfiguration.java
+++ bacon-common/bacon-common-seata/src/main/java/com/github/thundax/bacon/common/seata/config/BaconSeataAutoConfiguration.java
@@ -3,17 +3,17 @@ package com.github.thundax.bacon.common.seata.config;
 import io.seata.rm.datasource.DataSourceProxy;
 import io.seata.spring.annotation.GlobalTransactionScanner;
 import javax.sql.DataSource;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 
-@Configuration(proxyBeanMethods = false)
+@AutoConfiguration
 @ConditionalOnClass({GlobalTransactionScanner.class, DataSourceProxy.class})
 @ConditionalOnProperty(value = "bacon.seata.enabled", havingValue = "true")
 @EnableConfigurationProperties(BaconSeataProperties.class)

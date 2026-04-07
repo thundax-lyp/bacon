2313fcb34a3bdf31c805f906577c515eee18f8cd
thundax-lyp <731378491@qq.com>
2026-04-07 14:39:54 +0800

    Test(inventory): 补充实体构造规则测试说明
    - 为 InventoryEntityConstructorArchitectureTest 增加 DisplayName\n- 便于测试报告直接表达规则语义

--- bacon-biz/bacon-inventory/bacon-inventory-interfaces/src/test/java/com/github/thundax/bacon/inventory/interfaces/architecture/InventoryEntityConstructorArchitectureTest.java
+++ bacon-biz/bacon-inventory/bacon-inventory-interfaces/src/test/java/com/github/thundax/bacon/inventory/interfaces/architecture/InventoryEntityConstructorArchitectureTest.java
@@ -1,11 +1,13 @@
 package com.github.thundax.bacon.inventory.interfaces.architecture;
 
 import com.github.thundax.bacon.common.test.architecture.NamingAndPlacementRuleSupport;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class InventoryEntityConstructorArchitectureTest {
 
     @Test
+    @DisplayName("domain.model.entity：仅允许一个边界构造器，且必须委托到全字段构造器")
     void inventoryAuditDeadLetterShouldUseSingleExplicitBoundaryConstructor() {
         NamingAndPlacementRuleSupport.entityShouldUseSingleExplicitBoundaryConstructor(
                 "com.github.thundax.bacon.inventory.domain.model.entity.InventoryAuditDeadLetter")

7c47a3c4bf2d556ceb37a4e6095ecc68d8a20b1d
thundax-lyp <731378491@qq.com>
2026-04-20 16:37:17 +0800

    Revert "Refactor(upms): 当前用户门面返回部门编码"
    This reverts commit 829ee18b3e479fc7655c4013d9ace4842b380e97.

--- bacon-biz/bacon-upms/bacon-upms-api/src/main/java/com/github/thundax/bacon/upms/api/response/UserFacadeResponse.java
+++ bacon-biz/bacon-upms/bacon-upms-api/src/main/java/com/github/thundax/bacon/upms/api/response/UserFacadeResponse.java
@@ -1,11 +1,18 @@
 package com.github.thundax.bacon.upms.api.response;
 
-public record UserFacadeResponse(
-        Long id,
-        String account,
-        String name,
-        String avatarStoredObjectNo,
-        String phone,
-        String departmentCode,
-        String avatarUrl,
-        String status) {}
+import com.github.thundax.bacon.upms.api.dto.UserDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserFacadeResponse {
+
+    private UserDTO user;
+
+    public static UserFacadeResponse from(UserDTO user) {
+        return new UserFacadeResponse(user);
+    }
+}

1451f39e5141c6f0ab933bec9178d89948e447fd
thundax-lyp <731378491@qq.com>
2026-04-21 13:44:44 +0800

    Refactor(common-core): 新增通用分页查询参数PageQuery

---
+++ bacon-common/bacon-common-core/src/main/java/com/github/thundax/bacon/common/core/result/PageQuery.java
@@ -0,0 +1,21 @@
+package com.github.thundax.bacon.common.core.result;
+
+import com.github.thundax.bacon.common.core.util.PageParamNormalizer;
+import lombok.Getter;
+
+/**
+ * 通用分页查询参数。
+ */
+@Getter
+public class PageQuery {
+
+    /** 请求页码。 */
+    private Integer pageNo;
+    /** 请求每页大小。 */
+    private Integer pageSize;
+
+    public PageQuery(Integer pageNo, Integer pageSize) {
+        this.pageNo = PageParamNormalizer.normalizePageNo(pageNo);
+        this.pageSize = PageParamNormalizer.normalizePageSize(pageSize);
+    }
+}

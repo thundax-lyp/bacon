02bee2fdd285c5b566b2344240882977709a7e1e
Thundax <thundax@qq.com>
2026-04-23 21:22:59 +0800

    Fix(order): 修复订单分页请求参数绑定

--- bacon-biz/bacon-order/bacon-order-interfaces/src/main/java/com/github/thundax/bacon/order/interfaces/request/OrderPageRequest.java
+++ bacon-biz/bacon-order/bacon-order-interfaces/src/main/java/com/github/thundax/bacon/order/interfaces/request/OrderPageRequest.java
@@ -9,8 +9,10 @@ import java.time.Instant;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class OrderPageRequest {

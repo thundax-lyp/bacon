cd9115495354f211c71eacbfcce3fb328e3e0af9
Thundax <thundax@qq.com>
2026-04-23 21:59:09 +0800

    Fix(inventory): 补齐接口请求对象Setter

--- bacon-biz/bacon-inventory/bacon-inventory-interfaces/src/main/java/com/github/thundax/bacon/inventory/interfaces/request/InventoryBatchQueryRequest.java
+++ bacon-biz/bacon-inventory/bacon-inventory-interfaces/src/main/java/com/github/thundax/bacon/inventory/interfaces/request/InventoryBatchQueryRequest.java
@@ -7,8 +7,10 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class InventoryBatchQueryRequest {

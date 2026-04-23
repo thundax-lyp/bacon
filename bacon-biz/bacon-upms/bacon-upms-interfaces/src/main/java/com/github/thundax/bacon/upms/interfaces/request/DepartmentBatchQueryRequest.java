dc3b70287ce1b2f2e0ca351349b8b52f2ee0fad3
Thundax <thundax@qq.com>
2026-04-23 22:25:10 +0800

    Fix(upms): 补齐接口查询请求绑定能力

--- bacon-biz/bacon-upms/bacon-upms-interfaces/src/main/java/com/github/thundax/bacon/upms/interfaces/request/DepartmentBatchQueryRequest.java
+++ bacon-biz/bacon-upms/bacon-upms-interfaces/src/main/java/com/github/thundax/bacon/upms/interfaces/request/DepartmentBatchQueryRequest.java
@@ -5,8 +5,10 @@ import java.util.Set;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class DepartmentBatchQueryRequest {
